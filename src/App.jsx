e947ab83e947dd8798c8bf6e317cfa2bb89a02c3
PuneethMyadam <PuneethMyadam@users.noreply.github.com>
2025-05-27 07:50:44 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './Navbar'
+import Body from './body';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Body/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
